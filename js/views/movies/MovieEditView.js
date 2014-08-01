446e99211c46aef422a12e1e78e5bfa44b724cbe
Dariel Noel Vila Plagado <darielnoel@gmail.com>
2014-08-01 07:01:03 -0430

    testing images to localstorage

--- js/views/movies/MovieEditView.js
+++ js/views/movies/MovieEditView.js
@@ -109,9 +109,45 @@ define([
                     data: movie
                 });
                 viewSrcNode.html(template);
+
+                console.log(viewSrcNode.find('#files'));
+                viewSrcNode.find('#files').change(instance.handleFileSelect);
             }
 
             instance.show();
+        },
+
+        handleFileSelect: function(evt) {
+
+            console.log('handleFileSelect');
+            var files = evt.target.files; // FileList object
+
+            // Loop through the FileList and render image files as thumbnails.
+            for (var i = 0, f; f = files[i]; i++) {
+
+              // Only process image files.
+              if (!f.type.match('image.*')) {
+                continue;
+              }
+
+              var reader = new FileReader();
+
+              // Closure to capture the file information.
+              reader.onload = (function(theFile) {
+                return function(e) {
+                  // Render thumbnail.
+                  var span = document.createElement('span');
+                  span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                                    '" title="', escape(theFile.name), '"/>'].join('');
+                    
+                  document.getElementById('list').insertBefore(span, null);
+                  localStorage.setItem('img', e.target.result);
+                };
+              })(f);
+
+              // Read in the image file as a data URL.
+              reader.readAsDataURL(f);
+            }
         },
 
         /**
